87178021888ae16c4a1f2c51dd8f3c1202600052
Aleksander Piotrowski <pelotasplus@gmail.com>
2015-10-22 13:56:57 +0200

    initial commit

---
+++ Droid/MainApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace FormsDemo.Droid
+{
+	[Application]
+	public class MainApplication : Application
+	{
+		public MainApplication ()
+		{
+		}
+
+		public MainApplication (IntPtr handle, JniHandleOwnership transfer) : base (handle, transfer)
+		{
+		}
+
+		public override void OnCreate ()
+		{
+			base.OnCreate ();
+
+			ApplauseSDK.Applause.StartNewSession (this, APPLAUSE_APP_KEY);
+		}
+	}
+}
