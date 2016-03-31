57c9b6718068d20b6642d155bec7650006c2416c
Aleksander Piotrowski <pelotasplus@gmail.com>
2016-03-31 12:23:03 +0200

    Import Droid demo app for Xamarin.Android

---
+++ Droid/DroidDemo/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace DroidDemo
+{
+	[Application]
+	public class MainApp : Application
+	{
+		public MainApp (IntPtr javaReference, JniHandleOwnership transfer) : base (javaReference, transfer)
+		{
+		}
+
+		public override void OnCreate ()
+		{
+			base.OnCreate ();
+
+			ApplauseSDK.Configuration configuration = new ApplauseSDK.Configuration.Builder (this)
+				.WithAPIKey (APPLAUSE_APP_KEY)
+				.Build ();
+
+			ApplauseSDK.Applause.StartNewSession (this, configuration);
+		}
+	}
+}
